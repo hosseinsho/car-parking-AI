08a4edb6ccb7fb0e4cbc7030fb98e95cc67eb44f
hosseinsho <amiramiry125@gmail.com>
2022-06-06 22:19:33 +0430

    feat: add goal_test and a star to algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,7 @@
+from queue import PriorityQueue
+
+INF = 127
+
 
 def h_func(node):
     ret = 0
@@ -7,3 +11,39 @@ def h_func(node):
         if parking[car['row']][col] == 1:
             ret += 1
     return ret
+
+
+def goal_test(node):
+    return h_func(node) == 0
+
+
+def a_star_graph(start):
+    g = {start: 0}
+    h = {start: h_func(start)}
+    f = lambda node: g[node] + h[node]
+
+    closed_set = set()
+    open_set = PriorityQueue()
+    open_set.put((f(start), start))
+
+    while not open_set.empty():
+        u = open_set.get()[1]
+        if u in closed_set:
+            continue
+        if goal_test(u):
+            return g[u]
+
+        closed_set.add(u)
+        neighbors = u.neighbors()
+        for v in neighbors:
+            if v in closed_set:
+                continue
+            if v not in g:
+                h[v] = h_func(v)
+                g[v] = INF
+            new_g = g[u] + 1
+            if new_g < g[v]:
+                g[v] = new_g
+                open_set.put((f(v), v))
+
+    return -1
